f35b2ecdaf65fa43956dd30f14c8124bc4009e5a
Muyiwa Olarewaju <muyiwa.olarewaju@datamellon@gmail.com>
2023-01-15 15:47:12 +0100

    update main

--- lib/screen/profile/components/colors.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:ui';
-
-class ProfileColor {
-  static Color mainColor = Color(0xFF00AFB9);
-  static Color subColor = Color(0xFFFFFFFF);
-  static Color auxColor = Color(0xFFEB0909);
-  static Color textColor = Color(0xFF000000)
-}
